cd5993db3b95eb2bf76d77116b594e1adaff90bf
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2026-05-11 13:37:09 +0200

    Added SeatType model classes

--- models_b.py
+++ models_b.py
@@ -3,3 +3,16 @@ from typing import Optional
 
 # TODO: Student B - Definiši svoj SQLModel entitet ovdje
 # 
+
+
+class SeatType(SQLModel, table=True):
+	id: Optional[int] = Field(default=None, primary_key=True)
+	typeName: str
+
+
+class SeatTypeCreate(SQLModel):
+	typeName: str
+
+
+class SeatTypeUpdate(SQLModel):
+	typeName: Optional[str] = None
